a86694530a73fe07fc07940ac8c3da07de1067c0
guillermo.leal <guillermo01.galp@gmail.com>
2021-09-18 09:47:49 -0500

    ajuste diseño vista error404

--- src/client/src/views/error404.vue
+++ src/client/src/views/error404.vue
@@ -2,9 +2,24 @@
   <v-container fluid fill-height class="white">
     <v-layout align-center justify-center>
       <v-flex xs12 sm8 md4>
-        <h1>ERROR 404</h1>
-        <P>Page not found</P>
+        <h3 class="display-3 mb-2">ERROR 404</h3>
+        <p class="subtitle-1">
+          La página que estas buscando no existe o no está disponible. Intentalo
+          de nuevo o más tarde.
+        </p>
+        <v-btn color="primary" dark @click="goBack">regresar</v-btn>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
+
+<script>
+export default {
+  name: 'error404',
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    }
+  }
+};
+</script>
